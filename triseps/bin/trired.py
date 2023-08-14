86c96dba103519c8b280abd067f45fa663d164c6
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2023-08-14 22:13:06 +0900

    implement stack options

--- triseps/bin/trired.py
+++ triseps/bin/trired.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from astropy.table import QTable
+from astropy.stats import sigma_clipped_stats
 import astropy.io.fits as fits
 import sys
 
 from ..utils import pick, chop_reference_pixels, timestamp
 from ..dark import estimate_darkframe
 from ..flat import estimate_flatframe
+from ..warnings import eprint
 
 
 def main():
@@ -27,6 +29,12 @@ def main():
     '-q', '--ql', action='store_true',
     help='generate a stacked image cube for quick look')
   parser.add_argument(
+    '-s', '--stack', action='store_true',
+    help='generate a stacked image')
+  parser.add_argument(
+    '--track', action='store', type=str,
+    help='tracking file')
+  parser.add_argument(
     '-f', '--overwrite', action='store_true',
     help='overwrite the output file if exists')
   parser.add_argument(
@@ -40,10 +48,8 @@ def main():
 
   input = fits.open(args.fits)[0]
   frame_id = input.header['frameid']
-  area = pick(db, frame_id=frame_id)['effective_area']
-  data = chop_reference_pixels(input.data, area)
 
-  output = fits.PrimaryHDU(data=data, header=input.header)
+  output = fits.PrimaryHDU(data=None, header=input.header)
 
   dark_id = estimate_darkframe(db, frame_id)
   dark_hdu = hdul[dark_id]
@@ -52,12 +58,31 @@ def main():
   flat_hdu = hdul[flat_id]
 
   with timestamp(output):
+    def hist(message):
+      output.header.add_history(message)
+      eprint(f'INFO: {message}')
+
+    area = pick(db, frame_id=frame_id)['effective_area']
+    output.data = chop_reference_pixels(input.data, area)
+    hist(f'photo-sensitive area {area} is extracted.')
+
     output.data -= dark_hdu.data
-    output.header.add_history(f'dark current subtracted with "{dark_id}".')
+    hist(f'dark current subtracted with {dark_id}.')
+
     output.data /= flat_hdu.data
-    output.header.add_history(f'flat frame corrected with "{dark_id}".')
+    hist(f'flat frame corrected with {flat_id}.')
+
     if args.ql is True:
       assert output.data.ndim == 3
       output.data = output.data.mean(axis=0)
+      hist('image cube is stacked for quick look.')
+    elif args.stack is True:
+      assert output.data.ndim == 3
+      if args.track is None:
+        stats = sigma_clipped_stats(output.data, axis=0)
+        output.data = stats[0]
+        hist('3-sigma clipped mean is calculated.')
+      else:
+        pass
 
   output.writeto(args.output, overwrite=args.overwrite)
--- triseps/version.py
+++ triseps/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-version = '0.0.2'
+version = '0.0.3'
